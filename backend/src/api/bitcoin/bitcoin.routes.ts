e05df508be8d11b5e5575c6f242ce941e381cfbd
Mononaut <github@monospace.live>
2022-12-17 09:39:06 -0600

    support trees of RBF replacements

--- backend/src/api/bitcoin/bitcoin.routes.ts
+++ backend/src/api/bitcoin/bitcoin.routes.ts
@@ -615,7 +615,7 @@ class BitcoinRoutes {
 
   private async getRbfHistory(req: Request, res: Response) {
     try {
-      const replacements = rbfCache.getRbfChain(req.params.txId) || [];
+      const replacements = rbfCache.getRbfTree(req.params.txId) || null;
       const replaces = rbfCache.getReplaces(req.params.txId) || null;
       res.json({
         replacements,
@@ -628,7 +628,7 @@ class BitcoinRoutes {
 
   private async getRbfReplacements(req: Request, res: Response) {
     try {
-      const result = rbfCache.getRbfChains(false);
+      const result = rbfCache.getRbfTrees(false);
       res.json(result);
     } catch (e) {
       res.status(500).send(e instanceof Error ? e.message : e);
@@ -637,7 +637,7 @@ class BitcoinRoutes {
 
   private async getFullRbfReplacements(req: Request, res: Response) {
     try {
-      const result = rbfCache.getRbfChains(true);
+      const result = rbfCache.getRbfTrees(true);
       res.json(result);
     } catch (e) {
       res.status(500).send(e instanceof Error ? e.message : e);
